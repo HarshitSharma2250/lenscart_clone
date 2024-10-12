eb60bf92afc52253d036814f9e59a0166415bbe7
Harshit Sharma <sharma.harshit295@gmail.com>
2024-10-12 11:54:20 +0530

    cors

--- index.js
+++ index.js
@@ -11,13 +11,7 @@ const RatingRouter = require("./routes/rating_feedback.routes");
 
 // server initilizing
 const server = express();
-server.use(
-  cors({
-    origin: [""],
-    methods:["POST","GET"],
-    credentials: true
-  })
-);
+server.use( cors());
 
 
 server.get("/home", (_, res) => {
